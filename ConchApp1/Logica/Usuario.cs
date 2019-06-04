c653158c1b0becbba6c9baaf38fffc161d4926f1
ivanrosa06 <30476430+ivanrosa06@users.noreply.github.com>
2019-06-04 00:40:39 -0400

    creacion de clase usuario donde se agregaron los metodos de registro y mostrar datos de los respectivos usuarios

---
+++ ConchApp1/Logica/Usuario.cs
@@ -0,0 +1,70 @@
+﻿using ConchApp1.Models;
+using System;
+using System.Linq;
+
+namespace ConchApp1.Logica
+{
+    public class Usuario
+    {
+        CONCHAPPContext context = new CONCHAPPContext();
+        public void registrarUsuarioTransportista(Models.UsuariosTransportistas usuariosTransportistas)
+        {
+            try
+            {
+                context.UsuariosTransportistas.Add(usuariosTransportistas);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+
+        }
+
+        public void registrarUsuarioPasajero(Models.UsuarioPasajero usuarioPasajero)
+        {
+            try
+            {
+                context.UsuarioPasajeros.Add(usuarioPasajero);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+
+        }
+        public Models.UsuarioPasajero MostrarUsuarioPasajero(int id)
+        {
+            var pasajero = new Models.UsuarioPasajero();
+            try
+            {
+
+                pasajero = context.UsuarioPasajeros.Where(x => x.Id == id).Single();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            return pasajero;
+        }
+
+        public Models.UsuariosTransportistas MostrarUsuarioTransportista(int id)
+        {
+            var transportista = new Models.UsuariosTransportistas();
+            try
+            {
+
+                transportista = context.UsuariosTransportistas.Where(x => x.Id == id).Single();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            return transportista;
+        }
+    }
+}
+    
